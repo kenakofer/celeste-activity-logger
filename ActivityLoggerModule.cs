f3e7e4bdc59afe287be16da68a1477514bfd670d
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-12-17 11:40:55 -0800

    Update name to ActivityLogger

---
+++ ActivityLoggerModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.ActivityLogger {
+    public class ActivityLoggerModule : EverestModule {
+        
+        public static ActivityLoggerModule Instance;
+
+        public ActivityLoggerModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
